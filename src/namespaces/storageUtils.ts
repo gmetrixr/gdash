2bdd07cd81c2464e653f515672c8dc78d2884e7c
Sahil Ahuja <sahilahuja@gmail.com>
2022-06-14 13:39:56 +0000

    Adding getCookie fn

--- src/namespaces/storageUtils.ts
+++ src/namespaces/storageUtils.ts
@@ -1,39 +1,28 @@
 import Cookies from "js-cookie";
 
-
-//@sahil: Needs more checks. Not sure that we should be creating ismorphic cookie functions anymore
-// export function deleteTokenWithName({ key, req, res }: { key: string, req?: Request, res?: Response }) {
-//   if (IN_SERVER && req && res && req.cookies) {
-//     res.clearCookie(key);
-//   } else {
-//     return Cookies.remove(key);
-//   }
-// }
-
-
 export const permanentCookieOptionsBrowser = (jwtExpiryMinutes: number) => {
   //Expire cookie 1 minute before the token expires
   const expiry_in_days = (jwtExpiryMinutes - 1)/(60*24);
   return { expires: expiry_in_days };
 };
 
-
-export function setPermanentTokenWithName({ key, value, jwtExpiryMinutes, res }: { key: string, value: string, jwtExpiryMinutes?: number, res?: Response }) {
+/*
+* Below methods are CLIENT ONLY, so they are not isomorphic.
+**/
+export function setPermanentTokenWithName({ key, value, jwtExpiryMinutes }: { key: string, value: string, jwtExpiryMinutes?: number }) {
   if(jwtExpiryMinutes) {
     return Cookies.set(key, value, permanentCookieOptionsBrowser(jwtExpiryMinutes));
   }
   return Cookies.set(key, value);
 }
 
-export function getTokenWithName({ key, req }: { key: string, req?: Request }) {
+export function getPermanentCookieWithName({ key }: { key: string }): (string | undefined) {
   return Cookies.get(key);
 }
 
-
-/*
-* Below methods are CLIENT ONLY, so they are not isomorphic.
-*
-* */
+export function getTokenWithName({ key, req }: { key: string, req?: Request }) {
+  return Cookies.get(key);
+}
 
 export function setTemporaryTokenWithName({ key, value }: { key: string, value: string }) {
   return (typeof sessionStorage === "object")? sessionStorage.setItem(key, value): "";
